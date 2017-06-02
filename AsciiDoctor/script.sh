9c1d359ce709de41ca23b14ed509bcc5c3984ab2
tfabre <fabre.thibaut@gmail.com>
2017-06-02 13:43:13 +0200

    Re-insert Docker and script

---
+++ AsciiDoctor/script.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+echo $DIR_PDF/$DIR_THEME
+echo $DIR_PDF/$DIR_FONTS
+echo $DEFAULT_STYLE
+echo $DEFAULT_FILE
+
+asciidoctor-pdf -a pdf-stylesdir=$DIR_PDF/$DIR_THEME -a pdf-fontsdir=$DIR_PDF/$DIR_FONTS -a  pdf-style=$DEFAULT_STYLE $DEFAULT_FILE
