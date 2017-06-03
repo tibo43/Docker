0eee86ba35e2c46758f0733f17f67e22a6e51d7d
tfabre <fabre.thibaut@gmail.com>
2017-06-03 17:59:04 +0200

    Modification Dockerfile and script to clone the theme repo while the docker run

--- AsciiDoctor/script.sh
+++ AsciiDoctor/script.sh
@@ -1,7 +1,17 @@
 #/bin/bash
-echo $DIR_PDF/$DIR_THEME
-echo $DIR_PDF/$DIR_FONTS
-echo $DEFAULT_STYLE
-echo $DEFAULT_FILE
 
+check() {
+  if [ $? = 0 ] ; then
+    printf "\nGeneration OK."
+  else
+    printf "\nProblem while generation."
+  fi
+}
+
+printf "\nGit clone AsciiDoctor Theme repository"
+git clone https://github.com/tibo43/asciidoctor_theme.git /theme
+
+printf "\nExecute asciidoctor-pdf command\n"
 asciidoctor-pdf -a pdf-stylesdir=$DIR_PDF/$DIR_THEME -a pdf-fontsdir=$DIR_PDF/$DIR_FONTS -a  pdf-style=$DEFAULT_STYLE $DEFAULT_FILE
+
+check
